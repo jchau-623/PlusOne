7e19e584f968f2e06575a136a22c3aa327294a18
justin chau <jchau623@gmail.com>
2022-10-26 13:52:00 -0400

    validate subsequence

--- validateSubsequence.js
+++ validateSubsequence.js
@@ -3,3 +3,19 @@
 //  A subsequence of an array is a saet of numbers that aren't necessarily adjacent in the array but that are in the same order as
 // they appear in the array. For instance, the numbers [1, 3, 4] form a subsequence of the array [1, 2, 3, 4], and so do the numbers
 // [2, 4]. Note that a single number in an array and the array itself are both valid subsequences of the array.
+
+function isValidSubsequence(array, sequence) {
+    let arrIdx = 0;
+    let seqIdx = 0;
+
+    while (arrIdx < array.length && seqIdx < sequence.length){
+      if(array[arrIdx] === sequence[seqIdx]){
+        seqIdx++;
+      }
+      arrIdx++;
+    }
+
+    return seqIdx === sequence.length;
+
+
+  }
