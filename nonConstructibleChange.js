e13079bc598b51078e6c1a44e743581fe2fd9c17
justin chau <jchau623@gmail.com>
2022-11-01 14:19:02 -0400

    non constructible change

--- nonConstructibleChange.js
+++ nonConstructibleChange.js
@@ -4,3 +4,16 @@
 // create. The given coins can have
 // any positive integer value and aren't necessarily unique (i.e., you can have
 // multiple coins of the same value).
+
+function nonConstructibleChange(coins) {
+    coins.sort((a,b) => a - b)
+	let changeCreated = 0
+	for (let i = 0; i < coins.length; i++) {
+		let coin = coins[i]
+		if (coin > changeCreated + 1) return changeCreated + 1
+
+		changeCreated += coin;
+	}
+  return changeCreated + 1;
+  }
+ 
