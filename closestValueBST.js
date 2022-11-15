55cace221b294ed96be324519176753e918864bc
justin chau <jchau623@gmail.com>
2022-11-15 10:37:55 -0500

    BST

--- closestValueBST.js
+++ closestValueBST.js
@@ -1,1 +1,2 @@
 // Write a function that takes in a Binary Search Tree and a target integer value and returns the closest value to that target contained in the BST.
+// You can assume that there will only be one closest value.
