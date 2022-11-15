4c50868bd0ab35a9b2314e80fdf995081a18f1bb
justin chau <jchau623@gmail.com>
2022-11-15 10:39:27 -0500

    BST

--- closestValueBST.js
+++ closestValueBST.js
@@ -1,2 +1,5 @@
 // Write a function that takes in a Binary Search Tree and a target integer value and returns the closest value to that target contained in the BST.
 // You can assume that there will only be one closest value.
+// Each BST node has an integer value, a left child node, and a right child node. A node is said to be a valid BST node if and only if it satifies the
+// BST property: its value is strictly greater than the values of every node to its left; its value is less than or equal to the values of every node
+//  to its right; and its children nodes are either valid BST nodes themselves of None / null.
