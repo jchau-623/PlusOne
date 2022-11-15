d3921a8d0b3648f1feb9daad3951cd5a06d4481e
justin chau <jchau623@gmail.com>
2022-11-15 10:39:55 -0500

    BST

--- closestValueBST.js
+++ closestValueBST.js
@@ -3,3 +3,30 @@
 // Each BST node has an integer value, a left child node, and a right child node. A node is said to be a valid BST node if and only if it satifies the
 // BST property: its value is strictly greater than the values of every node to its left; its value is less than or equal to the values of every node
 //  to its right; and its children nodes are either valid BST nodes themselves of None / null.
+
+
+function findClosestValueInBst(tree, target) {
+    let closest = tree.value
+    let currentNode = tree
+    while (currentNode !== null) {
+      if (Math.abs(target - closest) > Math.abs(target - currentNode.value)) {
+      closest = currentNode.value
+    }
+      if (target < currentNode.value) {
+        currentNode = currentNode.left
+      } else if (target > currentNode.value) {
+        currentNode = currentNode.right
+      } else {
+        break
+      }
+    }
+    return closest
+  }
+  // This is the class of the input tree. Do not edit.
+  class BST {
+    constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+  }
