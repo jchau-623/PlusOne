bffbd9b97e1b0288ecfb9b77758ac4efc9fd21a6
justin chau <jchau623@gmail.com>
2023-04-07 14:25:58 -0400

    typescript unions

--- TypeScript/src/index.ts
+++ TypeScript/src/index.ts
@@ -29,10 +29,35 @@
 // console.log(mySize);
 
 
-function calculateTax (income: number, taxYear = 2022): number {
-    if ((taxYear || 2022) < 2022)
-        return income * 1.2;
-    return income * 1.3;
-}
+// function calculateTax (income: number, taxYear = 2022): number {
+//     if ((taxYear || 2022) < 2022)
+//         return income * 1.2;
+//     return income * 1.3;
+// }
 
-calculateTax(10_000)
+// calculateTax(10_000)
+
+// type Employee = {
+//     readonly id: number,
+//      name: string,
+//      retire: (date: Date) => void;
+// }
+
+// let employee: Employee = {
+// } =
+//     id: 1,
+//     name:'Mosh',
+//     retire: (date: Date) => {
+//     console.log(date)
+//     }
+"use strict"
+function kgToLbs(weight:number | string): number {
+    // weight.toString
+    if (typeof weight === 'number') {
+        return weight * 2.2
+    }
+    else
+        return parseInt(weight) * 2.2
+}
+console.log(kgToLbs(10))
+console.log(kgToLbs('10kg'))
