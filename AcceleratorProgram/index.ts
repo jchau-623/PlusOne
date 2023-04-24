15f293f259472d68749d6623da9219c32dffc9b9
justin chau <jchau623@gmail.com>
2023-04-24 14:06:25 -0400

    typescript intro

---
+++ AcceleratorProgram/index.ts
@@ -0,0 +1,21 @@
+let x = 4;
+let num : number = 6;
+let str: string = 'string';
+let bool: boolean = true;
+let variable: any = false;
+
+let isAnswered: boolean = true;
+
+console.log(x);
+
+
+let strArray: string[] = [];
+let strAr: Array<string> = [];
+
+let user : [number, string] = [9, "nein"];
+console.log(true)
+
+const small: number = 5;
+
+enum Size {small=5};
+console.log(Size)
