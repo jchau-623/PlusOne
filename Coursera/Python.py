ef8aafccaefa2c3ea2e2abbe91aed0d3c814e26a
justin chau <jchau623@gmail.com>
2024-01-31 11:43:21 -0500

    data science and ai

---
+++ Coursera/Python.py
@@ -0,0 +1,69 @@
+# print()	Prints the message or variable inside `()`.	Example:
+# 1
+# 2
+# print("Hello, world")
+# print(a+b)
+
+# Python Operators	- Addition (+): Adds two values together.
+# - Subtraction (-): Subtracts one value from another.
+# - Multiplication (*): Multiplies two values.
+# - Division (/): Divides one value by another, returns a float.
+# - Floor Division (//): Divides one value by another, returns the quotient as an integer.
+# - Modulo (%): Returns the remainder after division.	Example:
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# x = 9 y = 4
+# result_add= x + y # Addition
+# result_sub= x - y # Subtraction
+# result_mul= x * y # Multiplication
+# result_div= x / y # Division
+# result_fdiv= x // y # Floor Division
+# result_mod= x % y # Modulo</td>
+
+# replace()	Replaces substrings.	Example:
+# 1
+# 2
+# my_string="Hello"
+# new_text = my_string.replace("Hello", "Hi")
+
+# Slicing	Extracts a portion of the string.	Syntax:
+# 1
+# substring = string_name[start:end]
+
+# Example:
+
+# 1
+# my_string="Hello" substring = my_string[0:5]
+
+# split()	Splits string into a list based on a delimiter.	Example:
+# 1
+# 2
+# my_string="Hello"
+# split_text = my_string.split(",")
+
+# strip()	Removes leading/trailing whitespace.	Example:
+# 1
+# 2
+# my_string="Hello"
+# trimmed = my_string.strip()
+# upper()	Converts string to uppercase.	Example:
+# 1
+# 2
+# my_string="Hello"
+# uppercase_text = my_string.upper()
+
+# Variable Assignment	Assigns a value to a variable.	Syntax:
+# 1
+# variable_name = value
+
+# Example:
+
+# 1
+# 2
+# name="John" # assigning John to variable name
+# x = 5 # assigning 5 to variable x
